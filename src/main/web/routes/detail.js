97c548a80a8d4f1ce8fe6cf17f6b7786e7829c04
anfanger <zhangke.my.186@gmail.com>
2018-08-28 15:46:23 +0800

    中间测试

--- src/main/web/routes/detail.js
+++ src/main/web/routes/detail.js
@@ -34,12 +34,57 @@ router
         await ctx.render('./info/finance',{obj:data})
     })
     .get('/:id/risk',async(ctx,next)=>{
+        var option_1 = {
+            title: {
+                text: '条形图'
+            },
+            tooltip: {},
+            legend: {
+                data: ['销量']
+            },
+            xAxis: {
+                data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+            },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+            }]
+        };
+        let option_2={
+            title:{
+                text:'折线图'
+            },
+            tooltip:{},
+            legend:{
+                data:['净利润','销售费用']
+            },
+            xAxis:{
+                data:['2014年','2015年','2016年','2017年']
+            },
+            yAxis:{},
+            series:[{
+                name:'净利润',
+                type:'line',
+                data:[100,120,150,180]
+            },{
+                name:'销售费用',
+                type:'line',
+                data:[130,160,200,250]
+            }]
+        };
+        let dic={
+            test:'sdasd'
+        }
         let data={
             keys_1:[1,2,3],
             values_1:[[1,1,1],[2,2,2]],
             keys_2:[4,5,6,7],
-            values_2:[[3,3,3,3],[4,4,4,4]]
+            values_2:[[3,3,3,3],[4,4,4,4]],
+            d:'123'
         }
+
         await ctx.render('./info/risk',data);
     })
     .get('/:id/history',async(ctx,next)=>{
