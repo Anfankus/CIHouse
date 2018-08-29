e26d5d3a636608e98e28d757bf867310d32f2a4b
BabyPinocchio <1244281558@qq.com>
2018-08-29 10:42:41 +0800

    更新

--- src/main/web/public/javascripts/initial.js
+++ src/main/web/public/javascripts/initial.js
@@ -10,3 +10,28 @@ $(function(){
             iframe.css('width',iframe[0].contentDocument.body.scrollWidth)
         })
 })
+function clickmenu(id)
+{  
+    changecolor(id);
+    switch(id)
+    {case menu001 :menuclick();break;
+     case menu002 :menuclick();break;
+     case menu003 :menuclick();break;
+     default:   break;
+    }
+}
+
+//这个函数用于改变文字颜色
+function changecolor(id)
+{
+    menu001.style.color="white";
+    menu002.style.color="white";
+    menu003.style.color="white";
+    id.style.color="Aqua";
+}
+
+//用于控制"服务日志查询"的点击效果
+function menuclick()
+{
+    item.style.display="block"; 
+}
